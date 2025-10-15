2cb4aeaf6bed88af50abaface6a5bcc52bb2ca7d
saadullahkhan51 <saadullahkhan5151@gmail.com>
2025-10-16 00:13:26 +0500

    init change

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -2,10 +2,15 @@ from .payslip_generator import PayslipGenerator
 from .personal_tax_calculator import PersonalTaxCalculator
 from .annual_summary_generator import AnnualSummaryGenerator
 from .monthly_all_workers_generator import MonthlyAllWorkersGenerator
+from .skv4788_generator import SKV4788Generator
+from .skv4786_generator import SKV4786Generator
+
 
 __all__ = [
     'PayslipGenerator',
     'PersonalTaxCalculator',
     'AnnualSummaryGenerator',
-    'MonthlyAllWorkersGenerator'
+    'MonthlyAllWorkersGenerator',
+    'SKV4788Generator',
+    'SKV4786Generator'
 ]
